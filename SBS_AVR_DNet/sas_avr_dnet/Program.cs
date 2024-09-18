7f59b3e189b4e2e1a6c1f21aae5a9f088513104f
shirana <borajine02@gmail.com>
2024-09-18 22:04:27 +0900

    コマンドライン引数でSrc,Dstファイル指定に対応

--- SBS_AVR_DNet/sas_avr_dnet/Program.cs
+++ SBS_AVR_DNet/sas_avr_dnet/Program.cs
@@ -14,7 +14,26 @@ try
 }
 
 string srcPath_asmCodeFile = @".\SrcCode.sac";
-string dstPath_primitiveAsmCodeFile = @".\SrcCode.spac";
+string dstPath_primitiveAsmCodeFile = @".\SrcCode.psac";
+string dstPath_machineCodeFile = @".\out.hex";
+switch(args.Length){
+	case 1:
+		srcPath_asmCodeFile = args[0];
+		break;
+	case 2:
+		srcPath_asmCodeFile = args[0];
+		dstPath_primitiveAsmCodeFile = args[1];
+		break;
+	case 3:
+		srcPath_asmCodeFile = args[0];
+		dstPath_primitiveAsmCodeFile = args[1];
+		dstPath_machineCodeFile = args[2];
+		break;
+}
+if(args.Length == 0){ 
+}else if(args.Length ==1){
+}
+
 try{
 	AsmCodeLoader.MakePrimitiveAsmCodeFile(srcPath_asmCodeFile,dstPath_primitiveAsmCodeFile);
 }catch(Exception ex){
@@ -26,7 +45,7 @@ try{
 InstructionList codeInstructions;
 try
 {
-	codeInstructions = AsmCodeLoader.FromPrimitiveAsmCode(@".\SrcCode.spac",instructionDefinition);
+	codeInstructions = AsmCodeLoader.FromPrimitiveAsmCode(dstPath_primitiveAsmCodeFile,instructionDefinition);
 }catch(Exception ex){
 	Console.WriteLine("PrimitiveAsmCodeの翻訳に失敗しました。");
 	Console.WriteLine(ex.Message);
@@ -42,12 +61,13 @@ foreach(Instruction inst in codeInstructions.Instructions){
 
 string intelHex = IntelHex.ToIntelHex(codeInstructions.ToHexText());
 
-using(StreamWriter sr = new StreamWriter("./program.hex", false)){
+using(StreamWriter sr = new StreamWriter(dstPath_machineCodeFile, false)){
 	try
 	{
 		sr.Write(intelHex);
 		Console.WriteLine();
-		Console.WriteLine("output -> ./program.hex");
+		Console.Write("output -> ./program.hex");
+		Console.WriteLine(dstPath_machineCodeFile);
 	}catch(Exception){
 		Console.WriteLine("hexファイルの書き込みに失敗しました。");
 	}
--- SBS_AVR_DNet/sas_avr_dnet/obj/Debug/net6.0/sas_avr_dnet.AssemblyInfo.cs
+++ SBS_AVR_DNet/sas_avr_dnet/obj/Debug/net6.0/sas_avr_dnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sas_avr_dnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a97f2a8ad2f6fa95cbffd8db5be3012f1220ea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c578ac699823892f0b2aa838c7b40ade1caf8f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("sas_avr_dnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sas_avr_dnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
